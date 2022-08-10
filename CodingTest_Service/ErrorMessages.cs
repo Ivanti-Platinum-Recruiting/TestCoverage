1eb2fa08d3037ac09e8656845f956890d009492a
Martin Irwin <>
2022-08-10 16:45:14 +0100

    martin irwin solution

---
+++ CodingTest_Service/ErrorMessages.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CodingTest_Service
+{
+    public static class ErrorMessages
+    {
+        public const string USER_DOES_NOT_EXIST = "User does not exist";
+    }
+}
---
+++ CodingTest_Service/IRepositoryWrapper.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CodingTest_Service
+{
+    public interface IRepositoryWrapper
+    {
+        User GetUser(int id);
+    }
+}
